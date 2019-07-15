7695afe2c1872fc3444a82afebae922f30fb0805
David Andrews <dandrewsuk82@gmail.com>
2019-07-15 17:19:18 +0100

    Form success page added

--- src/components/form.js
+++ src/components/form.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ContactForm() {
     return (
         <div>
-            <form method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+            <form method="post" name="contact" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
               <input type="hidden" name="bot-field" />
                 <div className="row">
